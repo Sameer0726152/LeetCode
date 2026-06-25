26992b0f6acfd531a36e12da5f34c29ea7059287
Sameer Talekar <sameertalekar2007@gmail.com>
2026-06-26 01:05:28 +0530

    Time: 0 ms (100%), Space: 12.4 MB (52.76%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        write = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[write] = nums[i]
+                write += 1
+        return write
+
+
+        
