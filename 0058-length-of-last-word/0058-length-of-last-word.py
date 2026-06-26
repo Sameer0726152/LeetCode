733f03b84454c87d3a84bcb18c5c4066cea9cfce
Sameer Talekar <sameertalekar2007@gmail.com>
2026-06-26 19:04:25 +0530

    Time: 0 ms (100%), Space: 12.5 MB (16.93%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,7 +1,4 @@
 class Solution(object):
     def lengthOfLastWord(self, s):
-        idk = s.split()
-        word = idk[-1]
-        length = len(word)
-        return length
+        return len(s.split()[-1])
         
