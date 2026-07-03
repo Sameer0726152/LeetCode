863149aedbf068dcda40daf0e92964da8928c1de
Sameer Talekar <sameertalekar2007@gmail.com>
2026-07-03 16:50:31 +0530

    Time: 131 ms (33.35%), Space: 13.1 MB (47.23%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,15 +1,9 @@
 class Solution(object):
     def isPalindrome(self, s):
-        s = s.lower()
-        s1 = ""
-        s2 = ""
-        for item in s:
-            if item in "abcdefghijklmnopqrstuvwxyz1234567890":
-                s2 += item
-        for item in range(len(s2) - 1, -1, -1):
-            s1 += s2[item]
-        if s1 == s2:
-            return True
-        else:
-            return False
+        cleaned = ""
+        for char in s.lower():
+            if char.isalnum():
+                cleaned += char
+        return cleaned == cleaned[::-1]
+
         
