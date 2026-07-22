372872ad03e57099f946a148e053f92ef2d1d2f6
Sameer Talekar <sameertalekar2007@gmail.com>
2026-07-23 00:49:30 +0530

    Time: 1 ms (97.5%), Space: 47.1 MB (76.74%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int len = 2 * n;
+        int[] ans = new int[len];
+        for(int i = 0; i < n; i++){
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
