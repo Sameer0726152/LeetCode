3c846d2fb406ddab1d4ff9e6fc62a0f4d7552ad5
Sameer Talekar <sameertalekar2007@gmail.com>
2026-07-23 00:41:32 +0530

    Time: 0 ms (100%), Space: 46.7 MB (7.39%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] result = new int[nums.length];
+        int left = 0;
+        int right = n;
+        int i = 0;
+        while(left < n)
+        {
+            result[i] += nums[left];
+            i++;
+            result[i] += nums[right];
+            i++;
+            left++;
+            right++;
+        }
+        return result;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
