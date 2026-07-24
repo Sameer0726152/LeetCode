de1841be3cf60e804bf6b949db882cd02bd9e9e1
Sameer Talekar <sameertalekar2007@gmail.com>
2026-07-24 18:47:09 +0530

    Time: 148 ms (5.02%), Space: 47.9 MB (38.43%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -3,13 +3,11 @@ class Solution {
         int index = 0;
         for(int i = 0; i < nums.length; i++){
             if(nums[i] != 0){
-                nums[index] = nums[i];
-                index++;
+                nums[index++] = nums[i];
             }
         }
         while(index < nums.length){
-            nums[index] = 0;
-            index++;
+            nums[index++] = 0;
         }
         for(int i : nums){
             System.out.print(i + " ");
