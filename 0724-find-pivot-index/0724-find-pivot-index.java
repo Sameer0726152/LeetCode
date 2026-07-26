d7f9d6d99c72ce64bf49318cffdde9523a7f64d4
Sameer Talekar <sameertalekar2007@gmail.com>
2026-07-26 18:21:52 +0530

    Time: 1 ms (97.84%), Space: 47.1 MB (93.94%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int[] prefix = new int[nums.length];
+        prefix[0] = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            prefix[i] = prefix[i - 1] + nums[i];
+        }
+        for(int i = 0; i < nums.length; i++){
+            if(i == 0){
+                if(prefix[nums.length - 1] - prefix[i] == 0){
+                    return i;
+                }
+            }
+            else{
+                if(prefix[nums.length - 1] - prefix[i] == prefix[i - 1]){
+                    return i;
+                }
+            }
+            
+        }
+        return -1;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
