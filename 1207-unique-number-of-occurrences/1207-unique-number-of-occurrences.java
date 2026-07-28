d820ebd1a8ab4c29737a33bd3733f3c16659d67c
Sameer Talekar <sameertalekar2007@gmail.com>
2026-07-28 10:31:15 +0530

    Time: 2 ms (98.1%), Space: 43.7 MB (34.2%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : arr){
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for(int key : map.keySet()){
+            int freq = map.get(key);
+            if(set.contains(freq)){
+                return false;
+            }
+            else{
+                set.add(freq);
+            }
+        }
+        return true;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
