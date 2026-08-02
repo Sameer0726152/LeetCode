e6cb6886a944544d12e33c5e4d3873abd28b7f9b
Sameer Talekar <sameertalekar2007@gmail.com>
2026-08-02 15:36:13 +0530

    Time: 0 ms (100%), Space: 42.7 MB (97.64%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,21 +1,7 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        Integer dp[][] = new Integer[piles.length][piles.length];
-        int difference  = solve(piles, 0, piles.length - 1, dp);
-        return difference > 0;
+        return true;
     }
-    private int solve(int[] piles, int left, int right, Integer[][] dp) {
-    if (left == right) {
-        return piles[left];
-    }
-    if(dp[left][right] != null){
-        return dp[left][right];
-    }
-    int takeLeft = piles[left] - solve(piles, left + 1, right, dp);
-    int takeRight = piles[right] - solve(piles, left, right - 1, dp);
-    dp[left][right] = Math.max(takeLeft, takeRight);
-    return Math.max(takeLeft, takeRight);
-}
 }
 
 // Synced seamlessly with LeetHub Pro
