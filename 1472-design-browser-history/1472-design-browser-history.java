1d689ad6f77577d0445ba80fbdb02d84823ff465
Sameer Talekar <sameertalekar2007@gmail.com>
2026-08-03 20:17:05 +0530

    Time: 48 ms (87.8%), Space: 52.5 MB (52.57%) - LeetHub

---
+++ 1472-design-browser-history/1472-design-browser-history.java
@@ -0,0 +1,54 @@
+class BrowserHistory {
+
+    class Node{
+        String url;
+        Node next;
+        Node prev;
+        Node(String url){
+            this.url = url;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    Node current;
+
+    public BrowserHistory(String homepage) {
+        current = new Node(homepage);
+    }
+    
+    public void visit(String url) {
+        Node newnode = new Node(url);
+        current.next = newnode;
+        newnode.prev = current;
+        current = newnode;
+    }
+    
+    public String back(int steps) {
+        while(steps > 0 && current.prev != null){
+            current = current.prev;
+            steps--;
+        }
+        return current.url;
+    }
+    
+    public String forward(int steps) {
+        while(steps > 0 && current.next != null){
+            current = current.next;
+            steps--;
+        }
+        return current.url;
+    }
+}
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory obj = new BrowserHistory(homepage);
+ * obj.visit(url);
+ * String param_2 = obj.back(steps);
+ * String param_3 = obj.forward(steps);
+ */
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
