cf9ec0b332d024d28de564d701e4d27a9a9d4a58
Sameer Talekar <sameertalekar2007@gmail.com>
2026-08-06 01:10:34 +0530

    Time: 5 ms (98.07%), Space: 130.6 MB (57.67%) - LeetHub

---
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNodes(ListNode head) {
+        ListNode current = head;
+        ListNode prev = null;
+        while(current != null){
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+        ListNode temp = head;
+        int max = temp.val;
+        while(temp != null && temp.next != null){
+            if(temp.next.val < max){
+                temp.next = temp.next.next;
+            }
+            else{
+                temp = temp.next;
+                max = temp.val;
+            }
+        }
+        current = head;
+        prev = null;
+        while(current != null){
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+        return head;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
