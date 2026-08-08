723cfed9c41e4ff60f4fd5db5236034de9a008b8
Sameer Talekar <sameertalekar2007@gmail.com>
2026-08-08 23:57:04 +0530

    Time: 107 ms (28.26%), Space: 77.3 MB (75.19%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -3,22 +3,39 @@ class Solution {
     public int largestRectangleArea(int[] heights) {
         Stack<Integer> stack = new Stack<>();
         int ans = 0;
-        for(int i = 0; i <= heights.length; i++){
-            int currheight = (i == heights.length) ? 0 : heights[i];
-            while(!stack.isEmpty() && heights[stack.peek()] > currheight){
-                int index = stack.pop();
-                int height = heights[index];
-                int width;
-                if(stack.isEmpty()){
-                    width = i;
-                }
-                else{
-                    width = i - stack.peek() - 1;
-                }
-                int area = height * width;
-                ans = Math.max(ans, area);
+        int n = heights.length;
+        int[] prev = new int[n];
+        int[] next = new int[n];
+        for(int i = 0; i < n; i++){
+            while(!stack.isEmpty() && heights[stack.peek()] >= heights[i]){
+                stack.pop();
+            }
+            if(stack.isEmpty()){
+                prev[i] = -1;
+            }
+            else{
+                prev[i] = stack.peek();
             }
             stack.push(i);
+        }
+        stack.clear();
+        for(int i = n - 1; i  >= 0; i--){
+            while(!stack.isEmpty() && heights[stack.peek()] >= heights[i]){
+                stack.pop();
+            }
+            if(stack.isEmpty()){
+                next[i] = n;
+            }
+            else{
+                next[i] = stack.peek();
+            }
+            stack.push(i);
+        }
+        for(int i = 0; i < n; i++){
+            int width = next[i] - prev[i] - 1;
+            int height = heights[i];
+            int area = width * height;
+            ans = Math.max(ans, area);
         }
         return ans;
     }
